68f1517bf23f10525f0a18368e4285e2c3673f49
AliceGuo9401 <45186543+AliceGuo9401@users.noreply.github.com>
2018-11-25 21:57:38 +0800

    Brute Force

---
+++ ValidParentheses.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+bool isValid(string s) {
+    map<char, char> convertTable{{')', '('}, {'}', '{'}, {']', '['}};
+    if (s.empty())
+    return true;
+
+    if (s.size() % 2 != 0)
+    return false;
+
+    else if (s.size() % 2 == 0) {
+        stack<char> charStack;                              //栈stack 定义在头文件stack中
+        int flag(0);
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                charStack.push(s[i]);                       //push(item)新元素压入栈顶
+            else if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+                if (charStack.empty())  /**/
+                    return false;
+                    
+                char temp = s[i];
+                if (convertTable[temp] == charStack.top())  //top()返回栈顶元素
+                    charStack.pop();                        //pop()删除栈顶元素
+                else if (convertTable[temp] != charStack.top()) {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if (flag == 1 || !charStack.empty())
+            return false;
+        return true;
+    }
+}
+
+int main() {
+    string s("){");
+    cout << isValid(s);
+    return 0;
+}
